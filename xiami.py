4e1cad051338d03745d79e2dfabdc1db72e13352
caosz <caosz11@mails.tsinghua.edu.cn>
2014-04-22 09:28:05 +0800

    Update xiami.py
    Changed the signin url. Cant't use xiami.com/task/signin.

--- xiami.py
+++ xiami.py
@@ -6,6 +6,7 @@ A simple script  used to signin xiami
 @author:caosz11@mails.tsinghua.edu.cn
 '''
 
+import re
 import urllib
 from urllib import parse
 from urllib import request
@@ -15,8 +16,8 @@ login_url = 'http://www.xiami.com/web/login'
 
 
 login_data = urllib.parse.urlencode({
-    'email' : 'Your email',
-    'password' : 'Your password',
+    'email' : '810233438@qq.com',
+    'password' : 'XXb1860Jase',
     # 抓包软件得出的数据。下面那个是登录 
     'LoginButton' : '%E7%99%BB%E5%BD%95',
 })
@@ -35,12 +36,33 @@ urllib.request.install_opener(opener)
 login_request = urllib.request.Request(login_url, login_data, login_headers)
 login_response = urllib.request.urlopen(login_request).read().decode()
 
-# This is the url for check in.抓包得出的
-checkin_url = 'http://www.xiami.com/task/signin'
-checkin_headers = {'Referer' : 'http://www.xiami.com/', 'User-Agent' : 'Opera/9.60'}
+# To find the url for check in.
+mid_headers = {'Referer' : 'http://www.xiami.com/profile', 'User-Agent' : 'Opera/9.60'}
+mid_request = urllib.request.Request('http://www.xiami.com/web', None, mid_headers)
+mid_response = urllib.request.urlopen(mid_request).read().decode()
+#print(mid_response)
 
-checkin_request = urllib.request.Request(checkin_url, None, checkin_headers)
-checkin_response = urllib.request.urlopen(checkin_request).read().decode()
+# To check if we have already signed in.
+days_pattern = re.compile(r'已连续签到(\d+)天')
+result_match = days_pattern.findall(mid_response)[0]
+if result_match:
+    print('success! ' + 'You have checked in ' + result_match + ' days')
+else:
+    # If not already signed in. This is the url for check in.
+    url_pattern = re.compile(r'<a class="check_in" href="/web/checkin/id/(\d        +)">每日签到</a>')
+    url_match  = url_pattern.findall(mid_response)[0]
+    checkin_url = 'http://www.xiami.com/web/checkin/id/' + url_match
 
-print('success!' + 'You have checked in ' + checkin_response + ' days')
+    checkin_headers = {'Referer' : 'http://www.xiami.com/web', 'User-Agent'         : 'Opera/9.60'}
 
+    checkin_request = urllib.request.Request(checkin_url, None, checkin_headers)
+    checkin_response = urllib.request.urlopen(checkin_request).read().decode()
+
+    # Check if we succeed
+    days_pattern = re.compile(r'已连续签到(\d+)天')
+    result_match = days_pattern.findall(checkin_response)[0]
+
+    if result_match:
+        print('success! ' + 'You have checked in ' + login_days + ' days')
+    else:
+        print('Fail! ' + 'Please contact caosz11@mails.tsinghua.edu.cn')
